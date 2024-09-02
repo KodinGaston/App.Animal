3b4b27e656704d65d9912db8dad3ab9f66ad6272
KodinGaston <contact@kodingaston.com>
2024-09-02 14:03:50 +0200

    Agregar archivos de proyecto.

---
+++ App.Animal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Animal // parent
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("Animal sounds");
+    }
+}
+public class Dog : Animal       // child
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog barks: WOOF WOOF!");
+    }
+}
+public class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat meows: Meow meow.");
+    }
+}
+public class Cow : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cow moos: Moo moo.");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();     // crea un objeto de tipo Animal
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        Animal myCow = new Cow();
+
+        myAnimal.animalSound();     // llama a animalSound
+        myDog.animalSound();
+        myCat.animalSound();
+        myCow.animalSound();
+    }
+}
